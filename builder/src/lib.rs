13721002b1f24fcc88eb5ad974b9e775dc8c312d
tbangase <ichi.toshi217@gmail.com>
2022-02-27 21:38:59 +0900

    wip builder -> test 6.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,120 @@
 use proc_macro::TokenStream;
+use syn::{self, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _parsed_input = syn::parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let tokens = quote!{
+        impl Command {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl CommandBuilder {
+            pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
+                Ok(Command{
+                    executable: self.executable.clone().unwrap(),
+                    args: self.args.clone().unwrap(),
+                    env: self.env.clone().unwrap(),
+                    current_dir: self.current_dir.clone().unwrap(),
+                })
+            }
+
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+        
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+        
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+        
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
+    };
+    TokenStream::from(tokens)
 }
+
+#[allow(dead_code)]
+struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+#[allow(dead_code)]
+impl Command {
+    pub fn builder() -> CommandBuilder {
+        CommandBuilder {
+            executable: None,
+            args: None,
+            env: None,
+            current_dir: None,
+        }
+    }
+}
+
+#[allow(dead_code)]
+struct CommandBuilder {
+    executable: Option<String>,
+    args: Option<Vec<String>>,
+    env: Option<Vec<String>>,
+    current_dir: Option<String>,
+}
+
+#[allow(dead_code)]
+impl CommandBuilder {
+    pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
+        Ok(Command{
+            executable: self.executable.clone().unwrap(),
+            args: self.args.clone().unwrap(),
+            env: self.env.clone().unwrap(),
+            current_dir: self.current_dir.clone().unwrap(),
+        })
+    }
+    pub fn executable(&mut self, executable: String) -> &mut Self {
+        self.executable = Some(executable);
+        self
+    }
+
+    pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+        self.args = Some(args);
+        self
+    }
+
+    pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+        self.env = Some(env);
+        self
+    }
+
+    pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+        self.current_dir = Some(current_dir);
+        self
+    }
+}
+
