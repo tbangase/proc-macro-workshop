bc7d01562e3356f7396f6866d1c4d55e0c81e6de
TosHiki <ichi.toshi217@gmail.com>
2022-04-30 00:57:06 +0900

    2022/04/29

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,120 +1,147 @@
 use proc_macro::TokenStream;
-use syn::{self, DeriveInput};
-use quote::quote;
+use syn::{
+    self, DeriveInput, Data, 
+    Fields, FieldsNamed, Type, 
+    PathArguments, GenericArgument
+};
+use quote::{quote, format_ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _parsed_input = syn::parse_macro_input!(input as DeriveInput);
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    let struct_name = input.ident;
+    let builder_name = format_ident!("{struct_name}Builder");
+    let fields = extract_fields(&input.data);
 
-    let tokens = quote!{
-        impl Command {
-            pub fn builder() -> CommandBuilder {
-                CommandBuilder {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+    let field_iter = fields.named.iter().map(|field| {
+        let ident = field.ident.clone();
+        let ty = field.ty.clone();
+        if is_option_type(&field.ty) {
+            quote! {
+                #ident: #ty
+            }
+        } else {
+            quote! {
+                #ident: Option<#ty>
+            }
+        }
+    });
+
+    let initial_field_iter = fields.named.iter().map(|field| {
+        let ident = field.ident.clone();
+        quote! {
+            #ident: None
+        }
+    });
+
+    let build_field_iter = fields.named.iter().map(|field| {
+        let ident = field.ident.clone();
+        if is_option_type(&field.ty) {
+            quote! {
+                #ident: self.#ident.clone()
+            }
+        } else {
+            quote! {
+                #ident: self.#ident.clone().ok_or(format!("Field {} not allowed None", stringify!(#ident)))?
+            }
+        }
+    });
+
+    let setter_iter = fields.named.iter().map(|field| {
+        let ident = field.ident.clone();
+        let ty = field.ty.clone();
+        // let wrapped_type = false;
+        if is_option_type(&field.ty) {
+            if let Some(wrapped_type) = unwrap_type(&ty) {
+                quote! {
+                    pub fn #ident(&mut self, #ident: #wrapped_type) -> &mut Self {
+                        self.#ident = Some(#ident);
+                        self
+                    }
+                }
+            } else {
+                panic!("Implementation cannot handle Option type. Code again")
+            }
+        } else {
+            quote! {
+                pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            }
+        }
+    });
+
+    let expanded = quote! {
+        pub struct #builder_name {
+            #(#field_iter),*
+        }
+
+        impl #struct_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#initial_field_iter),*
                 }
             }
         }
 
-        pub struct CommandBuilder {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
-        }
+        impl #builder_name {
+            #(#setter_iter)*
 
-        impl CommandBuilder {
-            pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
-                Ok(Command{
-                    executable: self.executable.clone().unwrap(),
-                    args: self.args.clone().unwrap(),
-                    env: self.env.clone().unwrap(),
-                    current_dir: self.current_dir.clone().unwrap(),
+            pub fn build(&self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+                Ok(#struct_name {
+                    #(#build_field_iter),*
                 })
-            }
-
-            pub fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-        
-            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-        
-            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-        
-            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
             }
         }
     };
-    TokenStream::from(tokens)
+
+    TokenStream::from(expanded)
 }
 
-#[allow(dead_code)]
-struct Command {
-    executable: String,
-    args: Vec<String>,
-    env: Vec<String>,
-    current_dir: String,
-}
-
-#[allow(dead_code)]
-impl Command {
-    pub fn builder() -> CommandBuilder {
-        CommandBuilder {
-            executable: None,
-            args: None,
-            env: None,
-            current_dir: None,
-        }
+fn extract_fields(data: &Data) -> &FieldsNamed {
+    match &*data {
+        Data::Struct(struct_data) => match &struct_data.fields {
+            Fields::Named(fields) => &fields,
+            _ => panic!("This macro is not supported for UnNamed or Unit Structures")
+        },
+        _ => panic!("This macro is only supported for Structures")
     }
 }
 
-#[allow(dead_code)]
-struct CommandBuilder {
-    executable: Option<String>,
-    args: Option<Vec<String>>,
-    env: Option<Vec<String>>,
-    current_dir: Option<String>,
+fn is_option_type(ty: &Type) -> bool {
+    match ty {
+        Type::Path(type_path) => {
+            if let Some(path_segment) = type_path.path.segments.iter().next() {
+                if path_segment.ident == "Option" {
+                    return true
+                }
+            };
+        },
+        _ => panic!("unexpected field type")
+    }
+    false
 }
 
-#[allow(dead_code)]
-impl CommandBuilder {
-    pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
-        Ok(Command{
-            executable: self.executable.clone().unwrap(),
-            args: self.args.clone().unwrap(),
-            env: self.env.clone().unwrap(),
-            current_dir: self.current_dir.clone().unwrap(),
-        })
+fn unwrap_type(ty: &Type) -> Option<&Type> {
+    match ty {
+        Type::Path(type_path) => {
+            if let Some(path_segment) = type_path.path.segments.iter().next() {
+                match &path_segment.arguments {
+                    PathArguments::AngleBracketed(gen_args) => {
+                        match gen_args.args.last()? {
+                            GenericArgument::Type(ty) => {
+                                return Some(&ty)
+                            },
+                            _ => return None 
+                        }
+                    },
+                    _ => return None 
+                }
+            };
+        },
+        _ => return None 
     }
-    pub fn executable(&mut self, executable: String) -> &mut Self {
-        self.executable = Some(executable);
-        self
-    }
-
-    pub fn args(&mut self, args: Vec<String>) -> &mut Self {
-        self.args = Some(args);
-        self
-    }
-
-    pub fn env(&mut self, env: Vec<String>) -> &mut Self {
-        self.env = Some(env);
-        self
-    }
-
-    pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
-        self.current_dir = Some(current_dir);
-        self
-    }
+    None
 }
 
